7a57d5069adc20b4131ab1586a6218a724b8b841
Ee Venn Soh <esoh@atlassian.com>
2026-05-29 13:49:11 +1000

    Add skills directory block

---
+++ components/website/demos/blocks/skills-directory-demo.tsx
@@ -0,0 +1,5 @@
+import SkillsDirectoryPage from "@/components/blocks/skills-directory/page";
+
+export default function SkillsDirectoryDemo(): React.ReactElement {
+	return <SkillsDirectoryPage />;
+}
