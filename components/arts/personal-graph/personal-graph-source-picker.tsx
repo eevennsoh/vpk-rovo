0d0f5e47dc9cda4dec2156aed73c1ed118215e8e
Ee Venn Soh <esoh@atlassian.com>
2026-05-30 13:30:39 +1000

    Use animated TWG loader as graph connect button icon

--- components/arts/personal-graph/personal-graph-source-picker.tsx
+++ components/arts/personal-graph/personal-graph-source-picker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { TWGLoader } from "@/components/ui-custom/twg-loader";
 import { PixelVaultIcon } from "./personal-graph-pixel-icons";
 
 interface PersonalGraphSourcePickerProps {
@@ -37,14 +37,10 @@ export function PersonalGraphSourcePicker({
 				size="sm"
 				variant="outline"
 			>
-				<Image
-					alt=""
-					aria-hidden
-					className="size-4 shrink-0"
-					data-icon="inline-start"
-					height={16}
-					src="/icons/twg.svg"
-					width={16}
+				<TWGLoader
+					className="shrink-0"
+					label=""
+					size="small"
 				/>
 				Connect Team Work Graph
 			</Button>
